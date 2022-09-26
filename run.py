b641f1382b3dcc3ec8e1624eb024738143e428c5
Adrian Ong <ongadrian98@gmail.com>
2022-09-27 00:01:48 +0800

    Added new columns

--- run.py
+++ run.py
@@ -16,9 +16,19 @@ db = SQLAlchemy(app)
 
 class CarParkInfo(db.Model):
     __tablename__ = 'carparkinfo'
-    id = db.Column(db.Integer, primary_key=True)
-    carpark_number = db.Column(db.String(4), nullable=False)
+    carpark_number = db.Column(db.String(4), primary_key=True)
     address = db.Column(db.String(255), nullable=False)
+    x_coord = db.Column(db.Float, nullable=False)
+    y_coord = db.Column(db.Float, nullable=False)
+    carpark_type = db.Column(db.String(50), nullable=False)
+    electronic_parking_system = db.Column(db.Boolean, nullable=False)
+    short_term_parking = db.Column(db.String(255), nullable=False)
+    free_parking = db.Column(db.String(255), nullable=True)
+    night_parking = db.Column(db.Boolean, nullable=False)
+    carpark_deck_number = db.Column(db.Integer, nullable=False)
+    gantry_height = db.Column(db.Float(255), nullable=True)
+    carpark_basement = db.Column(db.Boolean, nullable=False)
+    avabilities = db.relationship('CarParkAvailability', backref='carparkinfo', lazy=True)
 
     def __eq__(self, other_instance):
         a = self.__dict__
@@ -40,9 +50,22 @@ class CarParkInfo(db.Model):
         return CarParkInfo.query.filter_by(carpark_number=carpark_number).first()
 
     @staticmethod
+    def get_all():
+        return CarParkInfo.query.all()
+
+    @staticmethod
     def update(existing_record, new_record):
-        existing_record.carpark_number = new_record.carpark_number
         existing_record.address = new_record.address
+        existing_record.x_coord = new_record.x_coord
+        existing_record.y_coord = new_record.y_coord
+        existing_record.carpark_type = new_record.carpark_type
+        existing_record.electronic_parking_system = new_record.electronic_parking_system
+        existing_record.short_term_parking = new_record.short_term_parking
+        existing_record.free_parking = new_record.free_parking
+        existing_record.night_parking = new_record.night_parking
+        existing_record.carpark_deck_number = new_record.carpark_deck_number
+        existing_record.gantry_height = new_record.gantry_height
+        existing_record.carpark_basement = new_record.carpark_basement
 
         db.session.commit()
 
@@ -58,22 +81,42 @@ class CarParkInfo(db.Model):
                 # Check if record is already in database
                 try:
                     # Record does not exist
-                    new_record = CarParkInfo(id=record['_id'],
-                                             carpark_number=record['car_park_no'],
-                                             address=record['address'])
+                    new_record = CarParkInfo(carpark_number=record['car_park_no'],
+                                             address=record['address'],
+                                             x_coord=record['x_coord'],
+                                             y_coord=record['y_coord'],
+                                             carpark_type=record['car_park_type'],
+                                             electronic_parking_system=1 if record['type_of_parking_system'] == "ELECTRONIC PARKING" else 0,
+                                             short_term_parking=record['short_term_parking'],
+                                             free_parking=record['free_parking'],
+                                             night_parking=1 if record['night_parking'] == "YES" else 0,
+                                             carpark_deck_number=record['car_park_decks'],
+                                             gantry_height=record['gantry_height'],
+                                             carpark_basement=1 if record['car_park_basement'] == "Y" else 0
+                                             )
                     new_record.save()
                 except exc.IntegrityError as e:
                     db.session.rollback()
                     # Record already exists
                     # Check if record is different
-                    new_record = CarParkInfo(id=record['_id'],
-                                             carpark_number=record['car_park_no'],
-                                             address=record['address'])
+                    new_record = CarParkInfo(carpark_number=record['car_park_no'],
+                                             address=record['address'],
+                                             x_coord=record['x_coord'],
+                                             y_coord=record['y_coord'],
+                                             carpark_type=record['car_park_type'],
+                                             electronic_parking_system=1 if record['type_of_parking_system'] == "ELECTRONIC PARKING" else 0,
+                                             short_term_parking=record['short_term_parking'],
+                                             free_parking=record['free_parking'],
+                                             night_parking=1 if record['night_parking'] == "YES" else 0,
+                                             carpark_deck_number=record['car_park_decks'],
+                                             gantry_height=record['gantry_height'],
+                                             carpark_basement=1 if record['car_park_basement'] == "Y" else 0
+                                             )
                     if (existing_record := CarParkInfo.get(record['car_park_no'])) != new_record:
                         # Record is different
                         # Update record
                         CarParkInfo.update(existing_record, new_record)
-                        print(f"Updated record of CarPark Number: {new_record.carpark_number}")
+                        print(f"{existing_record.carpark_number} is updated")
 
 
 class CarParkAvailability(db.Model):
@@ -110,12 +153,13 @@ class CarParkAvailability(db.Model):
                                                  timestamp=datetime.strptime(item['update_datetime'], "%Y-%m-%dT%H:%M:%S"))
                     record.save()
                 except exc.IntegrityError as e:
-                    print(f"Record id {e.params[0]} already exists, rolling back")
                     db.session.rollback()
+                    print(f"Record id {e.params[0]} already exists, rolling back")
 
 
 def short_term_parking_HDB_car(time_to_from, carpark_number, eps):
-    # Source: https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges
+    # Fare Source: https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges
+    # EPS Source: https://www.hdb.gov.sg/car-parks/shortterm-parking/electronic-parking
 
     # Convert into datetime objects
     from_time = datetime.strptime(time_to_from[0], "%Y-%m-%dT%H:%M")
@@ -133,17 +177,16 @@ def short_term_parking_HDB_car(time_to_from, carpark_number, eps):
     # 2. (Other hours) -> $0.60/1/2hr
     # 3. Night parking scheme capped at $5 per night (10:30pm to 7:00am)
 
-    # Hours start from 0 to 23
-    central_expensive_time_range = (time(6, 0), time(15, 59))
+    # Define time ranges
+    central_expensive_time_range = (time(7, 0), time(16, 59))
 
     # Night parking range
-    night_parking_time_range = (time(21, 30), time(5, 59))
+    night_parking_time_range = (time(22, 30), time(6, 59))
 
     counter = 0
     expensive_time_counter_minute = 0
     night_parking_counter_minute = 0
     while counter < total_minutes:
-        counter += 1
         datetime_now = from_time + timedelta(minutes=counter)
         if central_expensive_time_range[0] <= datetime_now.time() <= central_expensive_time_range[1] and datetime_now.weekday() != 6:
             # Within expensive range
@@ -151,6 +194,7 @@ def short_term_parking_HDB_car(time_to_from, carpark_number, eps):
         if datetime_now.time() >= night_parking_time_range[0] or datetime_now.time() <= night_parking_time_range[1]:
             # Night parking
             night_parking_counter_minute += 1
+        counter += 1
 
     # Get non-expensive time counter
     non_expensive_time_counter_minute = total_minutes - expensive_time_counter_minute
@@ -191,17 +235,117 @@ def short_term_parking_HDB_car(time_to_from, carpark_number, eps):
     return total_cost
 
 
-def calculate_parking_fare_HDB(carpark_number, hours):
-    pass
+def short_term_parking_HDB_motorbike(time_to_from):
+    # Fare source: https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges
+    # $0.65 per lot for whole day (7:00am to 10:30pm) or whole night (10.30pm to 7.00am)
+
+    # Convert into datetime objects
+    from_time = datetime.strptime(time_to_from[0], "%Y-%m-%dT%H:%M")
+    to_time = datetime.strptime(time_to_from[1], "%Y-%m-%dT%H:%M")-timedelta(minutes=1)
+
+    # Get total time in minutes first
+    total_minutes = (to_time - from_time).total_seconds() / 60
+
+    # 10-minute grace period
+    if total_minutes <= 10:
+        return 0
+
+    # Define time ranges
+    day_time_range = (time(7, 0), time(22, 29))
+    # Night parking range
+    night_time_range = (time(22, 30), time(6, 59))
+
+    total_cost = 0
+    # While loop
+    counter = 0
+    day = False
+    night = False
+    while counter <= total_minutes:
+        datetime_now = from_time + timedelta(minutes=counter)
+        if day_time_range[0] <= datetime_now.time() <= day_time_range[1]:
+            # Day
+            if not day and not night:
+                # First day
+                day = True
+                total_cost += 0.65
+            elif not day:
+                # Check if new day
+                day = True
+                night = False
+                total_cost += 0.65
+        elif datetime_now.time() >= night_time_range[0] or datetime_now.time() <= night_time_range[1]:
+            # Night
+            if not night and not day:
+                # First night
+                night = True
+                total_cost += 0.65
+            elif not night:
+                # Check if new night
+                day = False
+                night = True
+                total_cost += 0.65
+        counter += 1
+
+    return round(total_cost, 2)
+
+
+def short_term_parking_HDB_heavy(time_to_from, eps):
+    # Fare source: https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges
+    # $1.20 per half hour
+
+    # Convert into datetime objects
+    from_time = datetime.strptime(time_to_from[0], "%Y-%m-%dT%H:%M")
+    to_time = datetime.strptime(time_to_from[1], "%Y-%m-%dT%H:%M")
+
+    # Get total time in minutes first
+    total_minutes = (to_time - from_time).total_seconds() / 60
+
+    # 10-minute grace period
+    if total_minutes <= 10:
+        return 0
+
+    if eps:
+        # Pro-rated per minute
+        total_cost = round(total_minutes * (1.20 / 30), 2)
+    else:
+        # Assume coupons, count only every half hour
+        total_cost = round(math.ceil(total_minutes/30) * 1.20, 2)
+
+    return total_cost
+
+
+def get_top_carparks(xy_coords):
+    # 2 ways to get top carpark
+    # 1. Get top matches via Google Maps API
+    # 2. Get top matches via distance calculation in xy_coords, Distance squared = x squared + y squared
+
+    # Get all carparks locations
+    data = CarParkInfo.get_all()
+
+    print(data)
+
+    #distance_squared =
+
+
 
 
 if __name__ == '__main__':
-    # db.create_all()
-    # CarParkInfo.update_table()
-    # CarParkAvailability.update_table()
+    db.create_all()
+    CarParkInfo.update_table()
+    CarParkAvailability.update_table()
+
+    # xy_coords = (1.3601025254633325, 103.936328551415)
+    # print(get_top_carparks(xy_coords))
 
     # app.run(debug=True)
 
-    time_range = ("2021-03-01T12:00", "2021-03-02T12:00")
-    print(short_term_parking_HDB_car(time_range, "ACB", True))
+    # time_range = ("2021-03-01T07:00", "2021-03-02T07:00")
+    # print(short_term_parking_HDB_car(time_range, "ACB", True))
+    # print(short_term_parking_HDB_car(time_range, "ACB", False))
+    #
+    # time_range = ("2021-03-01T07:00", "2021-03-02T07:00")
+    # print(short_term_parking_HDB_motorbike(time_range))
+    #
+    # time_range = ("2021-03-01T07:00", "2021-03-02T07:00")
+    # print(short_term_parking_HDB_heavy(time_range, True))
 
